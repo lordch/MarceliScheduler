b92283bd1609a9aca67cd79f3853185f2e1da710
Michał Chełmiński <michalchelminski@gmail.com>
2022-06-25 19:02:04 +0200

    set up cron

--- cronjob.py
+++ cronjob.py
@@ -3,6 +3,6 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from main import synchronize_dbs
 
 scheduler = BlockingScheduler(timezone="Europe/Warsaw")
-scheduler.add_job(synchronize_dbs, "date", run_date="2022-06-25 18:40:05")
+scheduler.add_job(synchronize_dbs, "cron", hour=3)
 
 scheduler.start()
